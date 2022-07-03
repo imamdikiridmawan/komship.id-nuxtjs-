13bf5d01cd97957c46277d25513078249ca55a13
herlambangajiprasetyo <herlambangajiprasetyo36@gmail.com>
2022-07-03 14:20:55 +0700

    babel config

--- nuxt.config.js
+++ nuxt.config.js
@@ -92,5 +92,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {}
+  build: {},
+
+  // babel configuration
+  extend (config, ctx) {},
+  babel: { compact: true }
 }
